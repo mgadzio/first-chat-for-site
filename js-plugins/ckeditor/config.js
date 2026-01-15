c5165830850fd95e9cc9a7c39587438bb3b54a04
Marek Gadzała <marek.gadzala@upaid.pl>
2026-01-15 17:26:26 +0100

    changes

---
+++ js-plugins/ckeditor/config.js
@@ -0,0 +1,30 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	//config.toolbar = 'Basic';
+ 
+	config.toolbar_Basic =
+	[
+		{ name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline'] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','-','Blockquote'] },
+		'/',
+		{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+		{ name: 'colors', items : [ 'TextColor'] },
+		{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+	];
+	
+
+
+};
+
